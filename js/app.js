2bc6608233867e269900f0efd50e75c4d6a10f9a
issawiold <omar.m.issawi.95@gmail.com>
2023-05-31 11:15:10 +0000

    js people

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var name=prompt("kindly provide us with your name","anonymus");
+var gender=prompt("kindly provide us with your gender");
+if (gender!=("male"||"female")) {
+    gender=prompt("kindly provide us with your gender");
+};
+var age=prompt("kindly provide us with your age");
+if(age<=0) {
+    age=prompt("kindly provide us with your age") ;
+}
+var bol=!confirm("would you like us to skip the welcoming message");
+if (bol) {
+    if (gender=="male") {alert("welcome Mr."+name);
+        
+    }
+    else if (gender=="female") {alert("welcome Ms."+name);
+}
+    else{
+        alert("welcome Ms."+name);
+    }
+}
+
