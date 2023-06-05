9c46ddfcd4980d6156bb953ac50b0eb8a204b37f
issawiold <omar.m.issawi.95@gmail.com>
2023-06-05 14:27:28 +0000

    js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 var name=prompt("kindly provide us with your name","anonymus");
 var gender=prompt("kindly provide us with your gender");
-if (gender!=("male"||"female")) {
+if (!(gender==("male")||gender==("female"))) {
     gender=prompt("kindly provide us with your gender");
 };
 var age=prompt("kindly provide us with your age");
-if (this.age!="number") {
+if (typeof(age)!="number") {
     age=prompt("kindly provide us with your age");
 }
 if(age<=0) {
@@ -21,4 +21,25 @@ if (bol) {
         alert("welcome Ms."+name);
     }
 }
+let questions=["Have you ever sleepwalked\?","Do you like running\?","Do you know how to swim\?"]
+let answers=[]
+const usrInput=(arr)=>{for (let i = 0;i < arr.length;i++) {
+     let answer=window.prompt(arr[i]);
+    if(!(answer==("Yes") || answer==("No"))){
+        answers.push("invalid")
+    }
+    else{
+        answers.push(answer)
+    }
+    
+    
+}
+return
+}
+const printArray=(arr)=>{for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);  
+    
+}}
+usrInput(questions);
+printArray(answers)
 
