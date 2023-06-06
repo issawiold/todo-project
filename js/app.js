7ba76080c10770c5e1f3c14440a2094c3e9977c5
issawiold <omar.m.issawi.95@gmail.com>
2023-06-06 09:11:33 +0000

    js

--- js/app.js
+++ js/app.js
@@ -4,9 +4,6 @@ if (!(gender==("male")||gender==("female"))) {
     gender=prompt("kindly provide us with your gender");
 };
 var age=prompt("kindly provide us with your age");
-if (typeof(age)!="number") {
-    age=prompt("kindly provide us with your age");
-}
 if(age<=0) {
     age=prompt("kindly provide us with your age") ;
 }
@@ -21,25 +18,39 @@ if (bol) {
         alert("welcome Ms."+name);
     }
 }
-let questions=["Have you ever sleepwalked\?","Do you like running\?","Do you know how to swim\?"]
-let answers=[]
-const usrInput=(arr)=>{for (let i = 0;i < arr.length;i++) {
-     let answer=window.prompt(arr[i]);
-    if(!(answer==("Yes") || answer==("No"))){
-        answers.push("invalid")
+
+const usrInput=()=>{
+    let questions=["Have you ever sleepwalked\?","Do you like running\?","Do you know how to swim\?"]
+    let answers=[]
+    for (let i = 0;i < questions.length;i++) {
+    let answer=window.prompt(questions[i]);
+    answers.push(answer)
     }
-    else{
-        answers.push(answer)
-    }
-    
-    
+return answers
 }
-return
+const checkIfYesOrNo=(arr)=>{
+    let    newAnswers=[]
+    let    answer=[]
+    for (let i = 0; i < arr.length; i++) {
+        answer=arr[i];
+        if(!(answer==("Yes") || answer==("No"))){
+            newAnswers.push("invalid")
+        }
+        else{
+            newAnswers.push(answer)
+        }
+        
+    }
+
+    return newAnswers
+    
 }
 const printArray=(arr)=>{for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);  
     
 }}
-usrInput(questions);
-printArray(answers)
+
+let promptandStore=usrInput();
+let checkIfYesOrNo1=checkIfYesOrNo(promptandStore)
+printArray(checkIfYesOrNo1)
 
